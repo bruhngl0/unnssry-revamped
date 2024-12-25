3fd5fead8933bc1c5ca75311d7d82c1986e17ca6
bruhngl0 <adityashrm500@gmail.com>
2024-12-25 18:06:26 +0530

    pepe

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Main from './components/Main'
 import Jackets from './components/Jackets'
 import Jacketss from './components/Jacketss'
 import Shirts from './components/Shirts'
+import Pepe from './components/Pepe'
 
 import {BrowserRouter as Router, Routes, Route, BrowserRouter} from "react-router-dom"
 import Pants from './components/Pants'
@@ -20,14 +21,15 @@ function App() {
   return (
     <>
     <BrowserRouter>
-      <Header />
+<Header />
         <Routes>
           <Route path = "/" element = {<Main />} />
           <Route path = "/pants" element = {<Pants />} />  
           <Route path = "/jackets" element = {<Jacketss />} />  
           <Route path = "/shirts" element = {<Shirts />} />  
+          <Route path = "/pepe" element = {<Pepe />} />  
         </Routes>
-      <Footer />
+<Footer />
     </BrowserRouter>
     </>
   )
---
+++ src/components/Pepe.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import "../styles/pepe.scss"
+
+const Pepe = () => {
+  return (
+    <div className='pepe-top'>
+                      
+                     <div className='base-layer-7'>
+                        <div className='base-layer-7-1'></div>
+                        <div className='base-layer-7-2'></div>
+                    </div>
+
+                     <div className='base-layer-6'>
+                       
+                     </div>
+                <div className='base-layer-5'>
+                <div className='base-layer-5-1'>
+                    <div className='base-layer-5-1-1'></div>
+                    <div className='base-layer-5-1-2'></div>
+                </div>
+                </div>
+
+
+               <div className='base-layer-4'>
+
+               </div>
+               
+
+               <div className='base-layer-3'>
+                <div className='base-layer-3-1'></div>
+                <div className='base-layer-3-2'></div>
+               </div>
+               
+
+               <div className='base-layer-2'>
+               </div>
+
+
+               <div className='base-layer'>
+               </div>
+
+               
+    </div>
+  )
+}
+
+export default Pepe
