6bed25f039205c1cc82049106c1a3f708fa41dc6
bruhngl0 <adityashrm500@gmail.com>
2024-12-31 10:45:16 +0530

    styling-changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import "../styles/header.scss"
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div className='header-main'>
         <div className='header-left'>
+          <Link path to = "/">
             UNNSSRY
+            </Link>
         </div>
         <div className='header-mid'>  
             <span>NEW ARRIVALS / </span>
